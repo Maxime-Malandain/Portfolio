d26e5969103975c6efbbd85feeb431aa8159b7bc
Maxime Mld <m.malandain@hotmail.fr>
2023-09-07 00:24:19 +0200

    mise en place

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div>
+      <div className="container-footer">
+        <p>Conception et réalisation</p>
+        <p>Maxime Malandain</p>
+        <p>©2023, tous droits réservés</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+// import { useEffect, useState } from 'react';
+import Header from '../components/Layout/Header';
+import Footer from '../components/Layout/Footer';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>Maxime Malandain</h1>
+      <h2>Développeur Frontend Web et Web Mobile</h2>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
