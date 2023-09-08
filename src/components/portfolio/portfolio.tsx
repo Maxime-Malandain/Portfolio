331132d13b09739a05122f4df0da9cb487a1f1f3
Maxime Mld <m.malandain@hotmail.fr>
2023-09-08 10:45:59 +0200

    ✅ portfolio

--- src/components/portfolio/portfolio.tsx
+++ src/components/portfolio/portfolio.tsx
@@ -1,8 +1,167 @@
 import React from 'react';
 import './portfolio.css';
+import IMG1 from '../../assets/portfolio-1.jpeg';
 
-const portfolio = () => {
-  return <section id="portfolio">portfolio</section>;
-};
+function Portfolio() {
+  return (
+    <section id="portfolio">
+      <h5>Réalisations</h5>
+      <h2>Portfolio</h2>
 
-export default portfolio;
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Exemple de Projet" />{' '}
+            {/* //TODO Mettre le nom du projet */}
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>{' '}
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Exemple de Projet" />{' '}
+            {/* //TODO Mettre le nom du projet */}
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>{' '}
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Exemple de Projet" />{' '}
+            {/* //TODO Mettre le nom du projet */}
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>{' '}
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Exemple de Projet" />{' '}
+            {/* //TODO Mettre le nom du projet */}
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>{' '}
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Exemple de Projet" />{' '}
+            {/* //TODO Mettre le nom du projet */}
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>{' '}
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Exemple de Projet" />{' '}
+            {/* //TODO Mettre le nom du projet */}
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>{' '}
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+}
+
+export default Portfolio;
